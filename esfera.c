43acfa4e8903ec3d0d5c6421a9a8280f955e7cf2
rivail <araujorivail@outlook.com>
2021-03-15 17:46:38 -0300

    esfera ok

---
+++ esfera.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+ 
+int main() {
+    const double pi = 3.14159;
+    double raio, volume;
+    scanf("%lf", &raio);
+    volume = (4/3.0) * pi * raio*raio*raio;
+    printf("VOLUME = %.3lf\n", volume);
+
+    return 0;
+}
